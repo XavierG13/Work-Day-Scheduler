5e3157767c7979484e8050ef4df2dd3becef05d2
Xavier Guzman <guzmanxavi112@gmail.com>
2020-11-14 17:48:46 -0600

    added current day to page and js for save btn

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Work-Day-Scheduler
+
+## Task
+
+    For this project we were given the objective of creating a planner that would allow the user to enter and save events to a specific time frame. The user will also be able to view the present and past day via color code. The current day would also be displayed at the top of the page.
+
+## Purpose
+
+    This project is for us to be able to create an active planner using jQuery and the skills we learned in week 5. 
+
+    The current day and time will be set to the top of the page to users can see what day they are on. This will also update for each day that passes the new day will be set. 
+    Users will also be able to place their events in the respective time intervals. When the day or event has passed you will see that the color changes. This also goes for the day in the present and future.
+
+## 
+
--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var timeBlocksEl = document.getElementById("#time-blocks");
+$(document).ready(function () {
+
+    //here you will see that the current day and time are set to the page
+    var currentDayEl = document.getElementById("#currentDay");
+    currentDayEl = moment().format('MMMM Do YYYY, h:mm a');
+    $("#currentDay").append(currentDayEl);
+
+    //this listener is for when you hit the save icon
+    $(".saveBtn").on("click", function (event) {
+
+        var inputVal = $(this).siblings(".description");
+        console.log(inputVal);
+        var timeEl = $(this).parent().attr("id");
+
+        localStorage.setItem(inputVal, timeEl);
+        console.log(localStorage);
+
+    })
+
+    
 
 
-
-var hoursEl = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m."]
-
-for (var i = 0; i < hoursEl.length; i++) {
-    console.log(hoursEl);
-
-}
+});
